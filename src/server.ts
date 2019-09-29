08dfd1a856010ff1b98267332e1c3f5fc3567372
James Wright <james@jamesswright.co.uk>
2019-09-29 11:45:27 +0100

    Adds cart router with handler for creation endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,30 @@
 import express from 'express';
+import createInMemoryDataStore, { DataStore } from './data/dataStore';
+import { Cart } from './data/cart';
 
-const createServer = () => express();
+const createCartRouter = (carts: DataStore<Cart>) => {
+  const cartRouter = express.Router();
+
+  cartRouter.post('/', async (req, res) => {
+    const cart = await carts.save({ items: [] });
+
+    res.status(201).json(cart);
+  });
+
+  return cartRouter;
+};
+
+const createServer = () => {
+  const server = express();
+
+  server.use(
+    '/carts',
+    createCartRouter(
+      createInMemoryDataStore<Cart>(),
+    ),
+  );
+
+  return server;
+};
 
 export default createServer;
