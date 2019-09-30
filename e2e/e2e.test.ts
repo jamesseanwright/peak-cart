af1eb7e8468fe02724227730e3395c4864e8c42e
James Wright <james@jamesswright.co.uk>
2019-09-30 11:45:04 +0100

    Refactors E2E suite with reusable functions for calling the API

--- e2e/e2e.test.ts
+++ e2e/e2e.test.ts
@@ -1,25 +1,46 @@
 import { Application } from 'express';
 import request from 'supertest';
 import createServer from '../src/server';
-import { Cart } from '../src/data/cart';
-import { Record } from '../src/data/dataStore';
+import { Entity } from '../src/data/dataStore';
 import { Item } from '../src/data/item';
 
 const uuidFormat = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/i;
 
 const createCart = async (server: Application) => {
-  const response = await request(server)
+  const { status, body } = await request(server)
     .post('/carts')
     .set('Accept', 'application/json');
 
-  // TODO: unify this type assertion with app-wide function
-  const body = response.body as Record<Cart>;
-
   return {
-    status: response.status,
-    body,
+    status,
+    body: body as Entity,
   };
 };
+
+const getCartItems = async (server: Application, id: string) => {
+  const { status, body } = await request(server)
+    .get(`/carts/${id}/items`)
+    .set('Accept', 'application/json');
+
+  return {
+    status,
+    body: body as Item[]
+  }
+};
+
+const addToCart = async (server: Application, cartId: string, itemId: string) => {
+  return await request(server)
+    .patch(`/carts/${cartId}/items`)
+    .send({ id: itemId })
+    .set('Accept', 'application/json');
+};
+
+const replaceCartItems = async (server: Application, cartId: string, itemIds: string[]) => {
+  return await request(server)
+    .put(`/carts/${cartId}/items`)
+    .send({ items: itemIds })
+    .set('Accept', 'application/json');
+}
 
 describe('Cart API', () => {
   let server: Application;
@@ -40,17 +61,11 @@ describe('Cart API', () => {
   describe('/carts/:id/items', () => {
     it('should add an item to the cart when requested with HTTP PATCH', async () => {
       const { body } = await createCart(server);
-
-      const addItemResponse = await request(server)
-        .patch(`/carts/${body.id}/items`)
-        .send({ id: 'a9e9c933-eda2-4f45-92c0-33d6c1b495d8' })
-        .set('Accept', 'application/json');
+      const addItemResponse = await addToCart(server, body.id, 'a9e9c933-eda2-4f45-92c0-33d6c1b495d8');
 
       expect(addItemResponse.status).toBe(204);
 
-      const getItemsResponse = await request(server)
-        .get(`/carts/${body.id}/items`)
-        .set('Accept', 'application/json');
+      const getItemsResponse = await getCartItems(server, body.id);
 
       expect(getItemsResponse.status).toBe(200);
 
@@ -67,10 +82,7 @@ describe('Cart API', () => {
 
     it('should response with an empty array when it contains no items and requested with HTTP GET', async () => {
       const { body } = await createCart(server);
-
-      const itemsResponse = await request(server)
-        .get(`/carts/${body.id}/items`)
-        .set('Accept', 'application/json');
+      const itemsResponse = await getCartItems(server, body.id);
 
       expect(itemsResponse.status).toBe(200);
       expect(itemsResponse.body as Item[]).toEqual([]);
@@ -78,31 +90,20 @@ describe('Cart API', () => {
 
     it('should remove all items when requested with HTTP PUT and an empty array in the body', async () => {
       const { body } = await createCart(server);
-
-      const addItemResponse = await request(server)
-        .patch(`/carts/${body.id}/items`)
-        .send({ id: 'a9e9c933-eda2-4f45-92c0-33d6c1b495d8' })
-        .set('Accept', 'application/json');
+      const addItemResponse = await addToCart(server, body.id, 'a9e9c933-eda2-4f45-92c0-33d6c1b495d8');
 
       expect(addItemResponse.status).toBe(204);
 
-      const postAddCartResponse = await request(server)
-        .get(`/carts/${body.id}/items`)
-        .set('Accept', 'application/json');
+      const postAddCartResponse = await getCartItems(server, body.id);
 
       expect(postAddCartResponse.status).toBe(200);
       expect((postAddCartResponse.body as Item[])[0].title).toBe('The Testaments');
 
-      const clearItemsResponse = await request(server)
-        .put(`/carts/${body.id}/items`)
-        .send({ items: [] })
-        .set('Accept', 'application/json');
+      const clearItemsResponse = await replaceCartItems(server, body.id, []);
 
       expect(clearItemsResponse.status).toBe(204);
 
-      const postClearCartResponse = await request(server)
-        .get(`/carts/${body.id}/items`)
-        .set('Accept', 'application/json');
+      const postClearCartResponse = await getCartItems(server, body.id);
 
       expect(postClearCartResponse.status).toBe(200);
       expect((postClearCartResponse.body as Item[]).length).toBe(0);
@@ -110,30 +111,20 @@ describe('Cart API', () => {
 
     it('should respond with HTTP 400 when requested with HTTP PUT and a non-empty array', async () => {
       const { body } = await createCart(server);
-
-      const clearItemsResponse = await request(server)
-        .put(`/carts/${body.id}/items`)
-        .send({ items: [{ id: 'some_id' }] })
-        .set('Accept', 'application/json');
+      const clearItemsResponse = await replaceCartItems(server, body.id, ['some_id']);
 
       expect(clearItemsResponse.status).toBe(400);
     });
 
     it('should respond with HTTP 404 when the cart cannot be found', async () => {
-      const itemsResponse = await request(server)
-        .get(`/carts/missing_id/items`)
-        .set('Accept', 'application/json');
+      const itemsResponse = await getCartItems(server, 'missing_cart_id');
 
       expect(itemsResponse.status).toBe(404);
     });
 
     it('should respond with HTTP 400 when the item to add is unrecognised', async () => {
       const { body } = await createCart(server);
-
-      const addItemResponse = await request(server)
-        .patch(`/carts/${body.id}/items`)
-        .send({ id: 'foo' })
-        .set('Accept', 'application/json');
+      const addItemResponse = await addToCart(server, body.id, 'foo');
 
       expect(addItemResponse.status).toBe(400);
     });
